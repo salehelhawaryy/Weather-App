942be9f6f120e8ae8997c363fec01de00de5337c
salehelhawaryy <salehmohamad210@gmail.com>
2023-08-31 20:17:55 +0300

    added forecasting 3 days

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function loadLocation(Location){
         const temp = document.createElement('p');
         
         const searchbox = document.createElement('div');
-        searchbox.style="height: 30px;max-width: 200px;display: flex;justify-content: space-between;align-items: center;border-bottom: 2px solid black";
+        searchbox.style="height: 30px;max-width: 200px;display: flex;justify-content: space-between;align-items: center;border-bottom: 2px solid black; margin-top:90px";
         const inputText = document.createElement('input');
         inputText.placeholder = "Search Location..."
         inputText.style = "height: 100%;flex-shrink: 2;max-width: 150px;background-color: rgba(0,0,0,0);border: none;font-size: 15px;font-weight: 600;"
@@ -32,25 +32,25 @@ function loadLocation(Location){
         temp.textContent = resJSON.current.temp_c + ' °C';
         temp.classList.add('temp')
         time.textContent = resJSON.location.localtime;
-        time.style = "font-size:15px";
+        time.style = "font-size:20px";
         const condition = document.createElement('p');
         condition.classList.add('condition');
         condition.textContent = resJSON.current.condition.text;
         const location = document.createElement('p');
         location.textContent = resJSON.location.country + ', '+resJSON.location.name
-        location.style="font-size:15px";
+        location.style="font-size:20px";
         const windspeed = document.createElement('div')
-        windspeed.classList.add('wind-text','justify-content-start', 'pe-3')
-        windspeed.innerHTML = `<img src='../assets/wind.svg' style='width:40px !important;height:60px !important'><div><p class='mb-0'>Wind Speed</p><p class='mb-0' style='font-size:30px;font-weight:500'>${resJSON.current.wind_kph} Kp/h</p></div>`
+        windspeed.classList.add('wind-text')
+        windspeed.innerHTML = `<img src='../assets/wind.svg' style='width:40px !important;height:60px !important'><div><p class='mb-0'>Wind Speed</p><p class='mb-0' style='font-size:30px;font-weight:700'>${resJSON.current.wind_kph} Kp/h</p></div>`
         const feelsLike = document.createElement('div');
-        feelsLike.classList.add('wind-text','justify-content-start', 'pe-3')
-        feelsLike.innerHTML = `<img src='../assets/thermometer-sun.svg' style='width:50px !important;height:60px !important;'><div><p class='mb-0'>Feels Like</p><p class='mb-2' style='font-size:30px;font-weight:500'>${resJSON.current.feelslike_c}  °C</p></div>`
+        feelsLike.classList.add('wind-text')
+        feelsLike.innerHTML = `<img src='../assets/thermometer-sun.svg' style='width:50px !important;height:60px !important;'><div><p class='mb-0'>Feels Like</p><p class='mb-2' style='font-size:30px;font-weight:700'>${resJSON.current.feelslike_c}  °C</p></div>`
         const humidity = document.createElement('div')
-        humidity.classList.add('ano-text','justify-content-start', 'pe-3')
-        humidity.innerHTML = `<img src='../assets/humidity.png' style='width:40px !important;height:50px !important;'><div><p class='mb-0'>Humidity</p><p class='mb-2' style='font-size:30px;font-weight:500'>${resJSON.current.humidity}  %</p></div>`
+        humidity.classList.add('ano-text')
+        humidity.innerHTML = `<img src='../assets/humidity.png' style='width:40px !important;height:50px !important;'><div><p class='mb-0'>Humidity</p><p class='mb-2' style='font-size:30px;font-weight:700'>${resJSON.current.humidity}  %</p></div>`
         const rainChance = document.createElement('div')
-        rainChance.classList.add('ano-text','justify-content-start', 'pe-3')
-        rainChance.innerHTML = `<img src='../assets/rain.png' style='width:43px !important;height:44px !important;'><div><p class='mb-0'>Chance of Rain</p><p class='mb-2' style='font-size:30px;font-weight:500'>${resJSON.forecast.forecastday[0].day.daily_chance_of_rain}  %</p></div>`
+        rainChance.classList.add('ano-text')
+        rainChance.innerHTML = `<img src='../assets/rain.png' style='width:43px !important;height:44px !important;'><div><p class='mb-0'>Chance of Rain</p><p class='mb-2' style='font-size:30px;font-weight:700'>${resJSON.forecast.forecastday[0].day.daily_chance_of_rain}  %</p></div>`
         topRight.appendChild(feelsLike)
         topRight.appendChild(humidity)
         topRight.appendChild(rainChance)
@@ -60,6 +60,25 @@ function loadLocation(Location){
         topLeft.appendChild(time)
         topLeft.appendChild(temp)
         topLeft.appendChild(searchbox);
+
+        const actWeather = document.querySelector('.actWeather');
+        actWeather.innerHTML = ''
+        for (let i = 0 ; i < 3 ; i++){
+            const currDay = document.createElement('div')
+            currDay.style = "display:flex; flex-direction:column;justify-content:center"
+            const dateDay = document.createElement('p');
+            const tempDay = document.createElement('p');
+            dateDay.classList.add('fs-5')
+            tempDay.classList.add('text-center', 'fs-2', 'fw-bolder')
+            const conditionDay = document.createElement('img')
+            dateDay.textContent = resJSON.forecast.forecastday[i].date
+            tempDay.textContent = Math.round(resJSON.forecast.forecastday[i].day.avgtemp_c) + ' °C'
+            conditionDay.src = (('https:')+resJSON.forecast.forecastday[i].day.condition.icon);
+            currDay.appendChild(dateDay);
+            currDay.appendChild(tempDay);
+            currDay.appendChild(conditionDay);
+            actWeather.appendChild(currDay);
+        }  
     })
 }
 
